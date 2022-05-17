2c3e66170d39dc716bc4e5dce24ccdfc41024219
Tien Hung <hungntFX16329@funix.edu.vn>
2022-05-17 08:13:50 +0700

    commit 5

--- src/App.js
+++ src/App.js
@@ -13,6 +13,10 @@ class App extends Component {
       staffs : STAFFS
     };
   }
+  
+  ClickMe = ()=> {
+    alert ('You clicked me !')
+  }
   render(){
     return (
       <div>
@@ -21,6 +25,13 @@ class App extends Component {
             <NavbarBrand href="/">Ứng dụng quản lý nhân sự v1.0</NavbarBrand>
           </div>
         </Navbar>
+        <div className="container mt-2 mb-2">
+        <button onClick={this.ClickMe} type="button" className="btn btn-outline-primary">6 Column</button>
+        <button onClick={this.ClickMe} type="button" className="btn btn-outline-primary">4 Column</button>
+        <button onClick={this.ClickMe} type="button" className="btn btn-outline-primary">3 Column</button>
+        <button onClick={this.ClickMe} type="button" className="btn btn-outline-primary">2 Column</button>
+        <button onClick={this.ClickMe} type="button" className="btn btn-outline-primary">1 Column</button>
+        </div>
         <StaffList staffs={this.state.staffs}/> 
       </div>
     );
