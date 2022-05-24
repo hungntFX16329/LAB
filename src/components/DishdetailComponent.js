dc5ef3bd0a5be750341b5822867df3a2b8e015b6
Tien Hung <hungntFX16329@funix.edu.vn>
2022-05-24 14:57:17 +0700

    Lab 7 first commit

---
+++ src/components/DishdetailComponent.js
@@ -0,0 +1,80 @@
+import React from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+
+    function RenderDish({dish}){
+        return(
+            <Card>
+                 <CardImg width="100%" src={dish.image} alt={dish.name} />
+                 <CardBody>
+                    <CardTitle>{dish.name}</CardTitle>
+                    <CardText>{dish.description}</CardText>
+                 </CardBody>
+            </Card>
+        );
+    }
+
+
+    function RenderComment({commentArray}){
+        console.log(commentArray);
+        let comment = commentArray.map((comments)=>{
+            if (comments != null)
+                return (
+                    <div key={comments.id}>
+                        <ul style={{ listStyle: 'none',margin:0,padding:"10px"}}>
+                            <li>{comments.comment}</li>
+                            <li>--{comments.author} {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comments.date)))}</li>
+                        </ul>
+                    </div>
+                )
+            else {
+                return <div></div>
+            }
+        })
+        return (
+            <div className="container">
+                <div className="row"> 
+                    <h4>Comments</h4>
+                    <div>
+                    {comment}
+                    </div>
+                </div>
+            </div>
+        )
+        
+    }
+
+    const DishDetail = (props)=>{
+        if(props.dish !=null)
+            return (
+            <div className="container">
+                <div className="row">
+                    <Breadcrumb>
+                        <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                <div className="col-12">
+                    <h3>{props.dish.name}</h3>
+                    <hr />
+                </div>
+            </div>
+                <div className="row">
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderDish dish={props.dish} />
+                    </div>
+                    <div className="col-12 col-md-5 m-1">
+                        <RenderComment commentArray={props.comments} />
+                    </div>
+                </div> 
+            </div>
+        )
+        else {
+            return (
+                <div></div>
+            )
+        }
+    }
+
+
+export default DishDetail; 
