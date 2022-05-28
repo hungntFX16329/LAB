b58722a2e90631c680932bb462782d92e0f508d3
Tien Hung <hungntFX16329@funix.edu.vn>
2022-05-28 07:21:32 +0700

    5 commits

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,35 +5,38 @@ import NhanVien from './StaffListComponent';
 import PhongBan from './DepartmentComponent';
 import BangLuong from './SalaryComponent';
 import StaffDetail from './StaffDetailComponent'
-import { STAFFS} from '../shared/staffs';
-import { DEPARTMENTS } from '../shared/staffs';
-import { Switch, Route, Redirect} from 'react-router-dom';
+import { connect } from 'react-redux';
+import { Switch, Route, Redirect,withRouter} from 'react-router-dom';
 
+
+const mapStateToProps = state => {
+  return {
+    staffs: state.staffs,
+    departments: state.departments
+  }
+}
 
 class Main extends Component {
 
   constructor(props){
     super(props);
-    this.state = {
-      staffs : STAFFS,
-      departments: DEPARTMENTS
-    };
+    
     this.addStaff = this.addStaff.bind(this);
   }
 
   addStaff=(staff)=>{
-    let list = localStorage.getItem('userList')? JSON.parse(localStorage.getItem('userList')) : this.state.staffs;
+    let list = localStorage.getItem('userList')? JSON.parse(localStorage.getItem('userList')) : this.props.staffs;
     const id = Math.floor(Math.random()*1000+16);
     const newStaff = {id, ...staff};
     this.setState({
-      staffs: [...this.state.staffs,newStaff]
+      staffs: [...this.props.staffs,newStaff]
     });
     list.push(newStaff)
     localStorage.setItem('userList',JSON.stringify(list))
   }
 
   render(){
-    let list = localStorage.getItem('userList')? JSON.parse(localStorage.getItem('userList')) : this.state.staffs;
+    let list = localStorage.getItem('userList')? JSON.parse(localStorage.getItem('userList')) : this.props.staffs;
     const StaffWithId = ({match})=>{
       return (
         <StaffDetail staff={list.filter((staff)=>staff.id === parseInt(match.params.staffId,10))}/>
@@ -44,10 +47,10 @@ class Main extends Component {
       <div>
         <Header />
           <Switch>
-            <Route exact path="/nhanvien" component={()=><NhanVien onAdd={this.addStaff} staffs={this.state.staffs} />} />
+            <Route exact path="/nhanvien" component={()=><NhanVien onAdd={this.addStaff} staffs={this.props.staffs} />} />
             <Route path="/nhanvien/:staffId" component={StaffWithId}/>
-            <Route exact path="/phongban" component={()=><PhongBan department={this.state.departments}/>} />
-            <Route path="/bangluong" component={()=><BangLuong salary={this.state.staffs}/>} />
+            <Route exact path="/phongban" component={()=><PhongBan department={this.props.departments}/>} />
+            <Route path="/bangluong" component={()=><BangLuong salary={this.props.staffs}/>} />
             <Redirect to="/nhanvien" />
           </Switch>
         <Footer />
@@ -56,4 +59,4 @@ class Main extends Component {
 }
 }
 
-export default Main;
+export default withRouter(connect(mapStateToProps)(Main));
